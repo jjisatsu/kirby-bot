995d815977d5a127f116e5e2da856a265104bc34
jjisatsu <paulaplivcha@gmail.com>
2026-04-19 15:25:35 +0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+bot.py
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intens
+
+@bot.event
+async def on_ready():
+print (f"Kirbs is online as {bot.user}")
+
+@bot.command()
+async def hello(ctx):
+await ctx.send ("Kirbs says hey")
+bot.run(os.getenv("DISCORD_TOKEN"))
